823cc8a888e0af57370d8e2e6c44025ff1d301be
qianchu <hey_flora@126.com>
2021-02-18 07:50:26 +0000

    context tgtw baseline

--- preparation.sh
+++ preparation.sh
@@ -5,3 +5,4 @@ python ./preparation/generate_sent_cls.py
 python ./preparation/generate_sent_cls_ws.py
 python ./preparation/generate_token_cls.py
 python ./preparation/get_vocab.py
+python ./preparation/context_tgtw_baselines.py
---
+++ preparation/context_tgtw_baselines.py
@@ -0,0 +1,34 @@
+import sys
+
+eval_dataset = ['senseval2', 'senseval3', 'semeval2007', 'semeval2013', 'semeval2015', 'ALL']
+train_dataset = ['SemCor']
+
+file_path = []
+for dataset in eval_dataset:
+    file_path.append('./Evaluation_Datasets/' + dataset + '/' + dataset+'.csv')
+for dataset in train_dataset:
+    file_path.append('./Training_Corpora/' + dataset + '/' + dataset.lower()+'.csv')
+    
+for flag in ['c','w']:
+    for csv_file in file_path:
+
+        lines=open(csv_file).readlines()
+        for i in range(1,len(lines)):
+            sentence,tgt_i_start,tgt_i_end,tgt_id,tgt_lemma, tgt_pos,sense_key=lines[i].split('\t')
+            if flag=='c':
+                sentence=sentence.split()
+                del sentence[int(tgt_i_start):int(tgt_i_end)]
+                sentence=sentence[:int(tgt_i_start)]+['[MASK]']+sentence[int(tgt_i_start):]
+                sentence=' '.join(sentence)
+                tgt_i_start,tgt_i_end=tgt_i_start,str(int(tgt_i_start)+1)
+            elif flag=='w':
+                sentence=sentence.split()
+                w=sentence[int(tgt_i_start):int(tgt_i_end)]
+                sentence=' '.join(w)
+                tgt_i_start,tgt_i_end=str(0),str(len(w))
+
+            lines[i]='\t'.join([sentence,tgt_i_start,tgt_i_end,tgt_id,tgt_lemma, tgt_pos,sense_key])
+
+
+        with open(csv_file+'_'+flag+'.csv','w') as f:
+            f.write(''.join(lines))
