2cbc5d63a3009f07f9b158a14b8f0b57746cd011
qianchu <hey_flora@126.com>
2021-03-21 21:29:12 +0000

    eval data name

--- run_classifier_WSD_sent.py
+++ run_classifier_WSD_sent.py
@@ -654,7 +654,7 @@ def main():
         eval_loss, eval_accuracy = 0, 0
         nb_eval_steps, nb_eval_examples = 0, 0
 
-        with open(os.path.join(args.output_dir, "results.txt"),"w") as f:
+        with open(os.path.join(args.output_dir, "results_{0}.txt".format(os.path.basename(args.eval_data_dir).split('_')[0])),"w") as f:
             for input_ids, input_mask, segment_ids, label_ids in tqdm(eval_dataloader, desc="Evaluating"):
                 input_ids = input_ids.to(device)
                 input_mask = input_mask.to(device)
@@ -698,7 +698,7 @@ def main():
         result['global_step'] = global_step
         result['loss'] = loss
 
-        output_eval_file = os.path.join(args.output_dir, "eval_results.txt")
+        output_eval_file = os.path.join(args.output_dir,"eval_results_{0}.txt".format(os.path.basename(args.eval_data_dir).split('_')[0]))
         with open(output_eval_file, "a+") as writer:
             logger.info("***** Eval results *****")
             for key in result.keys():
