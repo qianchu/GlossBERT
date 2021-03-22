208b73eed1ca811958f62ba4a8211f8a02d5e7d4
qianchu <hey_flora@126.com>
2021-03-22 14:48:54 +0000

    wsd_random

---
+++ wsd_random.py
@@ -0,0 +1,17 @@
+import sys
+
+lines=open(sys.argv[1]).readlines()
+target_id_prev=None
+candidate_nums=[]
+candidate_num=None
+for line in lines[1:]:
+    target_id=line.split('\t')[0]
+    if target_id!=target_id_prev:
+        if candidate_num:
+            candidate_nums.append(candidate_num)
+        candidate_num=1
+
+    else:
+        candidate_num+=1
+
+print('mean candidate num',sum(candidate_nums)/len(candidate_nums))
