1866f725d50cdd6136f0bf046e991f83e15ed33d
qianchu <hey_flora@126.com>
2021-03-22 14:54:52 +0000

    wsd_random

--- wsd_random.py
+++ wsd_random.py
@@ -7,9 +7,11 @@ candidate_num=None
 for line in lines[1:]:
     target_id=line.split('\t')[0]
     if target_id!=target_id_prev:
+        target_id_prev=target_id
         if candidate_num:
             candidate_nums.append(candidate_num)
         candidate_num=1
+        
 
     else:
         candidate_num+=1
